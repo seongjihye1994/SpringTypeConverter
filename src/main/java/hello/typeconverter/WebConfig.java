eb2de66f0da6ccdd6df775d94e2d05449149808b
jihye.seong <jihye.seong@ipageon.com>
2022-04-13 00:55:37 +0900

    Spring Type Converter
    ConversionService addFormatters()를 사용해 컨버터 등록하기

---
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,28 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * WebMvcConfigurer 가 제공하는
+     * addFormatters() 를 사용해서
+     * 추가하고 싶은 컨버터를 등록
+     *
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
